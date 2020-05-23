a89771c365cb8a56d70dc4a82c764cee286400ae
Saurabh kacholiya <kacholiyasaurabh.com>
2020-05-23 22:50:07 +0530

    change publicPath / to /home/

--- internals/webpack/webpack.base.babel.js
+++ internals/webpack/webpack.base.babel.js
@@ -12,7 +12,7 @@ module.exports = options => ({
     {
       // Compile into js/build.js
       path: path.resolve(process.cwd(), 'build'),
-      publicPath: '/',
+      publicPath: '/home/',
     },
     options.output,
   ), // Merge with env dependent settings
